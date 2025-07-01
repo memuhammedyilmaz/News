a9d1dbd68a68bfa2d15eb6830693a1c7bdd30ed8
memuhammedyilmaz <yilmaz34muhammed@icloud.com>
2025-07-01 08:23:30 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Muhammed Yılmaz on 1.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
