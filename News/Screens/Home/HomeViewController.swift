ab50284014c9078caa8aeee15918593cb8abb933
memuhammedyilmaz <yilmaz34muhammed@icloud.com>
2025-07-14 21:40:24 +0300

    SearchBar Added

--- News/Screens/Home/HomeViewController.swift
+++ News/Screens/Home/HomeViewController.swift
@@ -16,7 +16,17 @@ class HomeViewController: UIViewController {
     
     private var viewModel = HomeViewModel()
     private var isLoading = true
-   
+    
+    private lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search News"
+        searchBar.searchBarStyle = .minimal
+        searchBar.backgroundImage = UIImage()
+        searchBar.delegate = self
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        return searchBar
+    }()
+    
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
         tableView.delegate = self
@@ -35,7 +45,7 @@ class HomeViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
@@ -44,14 +54,14 @@ class HomeViewController: UIViewController {
     }
     
     private func bindViewModel() {
-           viewModel.onDataUpdated = { [weak self] in
-               guard let self = self else { return }
-               DispatchQueue.main.async {
-                   self.isLoading = false
-                   self.tableView.reloadData()
-               }
-           }
-       }
+        viewModel.onDataUpdated = { [weak self] in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.isLoading = false
+                self.tableView.reloadData()
+            }
+        }
+    }
 }
 
 
@@ -65,11 +75,19 @@ extension HomeViewController {
     
     func addViews() {
         view.addSubview(tableView)
+        view.addSubview(searchBar)
     }
     
     func configureLayout() {
-        tableView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+            
         }
     }
 }
@@ -90,15 +108,15 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            if isLoading {
-                let cell = tableView.dequeueReusableCell(withIdentifier: SkeletonCell.identifier, for: indexPath) as! SkeletonCell
-                return cell
-            } else {
-                let cell = tableView.dequeueReusableCell(withIdentifier: NewsCell.identifier, for: indexPath) as! NewsCell
-                cell.configure(with: viewModel.articles[indexPath.row])
-                return cell
-            }
+        if isLoading {
+            let cell = tableView.dequeueReusableCell(withIdentifier: SkeletonCell.identifier, for: indexPath) as! SkeletonCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: NewsCell.identifier, for: indexPath) as! NewsCell
+            cell.configure(with: viewModel.articles[indexPath.row])
+            return cell
         }
+    }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
@@ -107,6 +125,21 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         navigationController?.pushViewController(detailVC, animated: true)
     }
     
-
+    
 }
 
+extension HomeViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+       let searchText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        if searchText.isEmpty {
+            viewModel.fetchArticles()
+        } else {
+            viewModel.searchArticles(with: searchText)
+        }
+    }
+    
+}
+
+#Preview {
+    HomeViewController()
+}
--- News/Screens/Home/HomeViewModel.swift
+++ News/Screens/Home/HomeViewModel.swift
@@ -10,18 +10,30 @@ import Foundation
 class HomeViewModel {
     
     private let networkManager = NetworkManager()
-        var articles: [Article] = []
-        var onDataUpdated: (() -> Void)?
-
-        func fetchArticles() {
-            networkManager.fetchArticles { [weak self] result in
-                switch result {
-                case .success(let newsModel):
-                    self?.articles = newsModel.articles
-                    self?.onDataUpdated?()
-                case .failure(let error):
-                    print("Error fetching articles: \(error)")
-                }
+    var articles: [Article] = []
+    var onDataUpdated: (() -> Void)?
+    
+    func fetchArticles() {
+        networkManager.fetchArticles { [weak self] result in
+            switch result {
+            case .success(let newsModel):
+                self?.articles = newsModel.articles
+                self?.onDataUpdated?()
+            case .failure(let error):
+                print("Error fetching articles: \(error)")
             }
         }
+    }
+    
+    func searchArticles(with: String) {
+        let filteredArticles = articles.filter { article in
+            guard let title = article.title else { return false }
+            return title.lowercased().contains(with.lowercased())
+        }
+        articles = filteredArticles
+        onDataUpdated?()
+    }
+    
+   
+    
 }
